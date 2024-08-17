3b0c9c7310a4abe35e72512147010b9d57edd033
tavanhai123 <haitienboi04@gmail.com>
2024-08-17 07:53:49 +0700

    Add BaiVeNha1

---
+++ BaiVeNha1/baitapbuoi1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+echo '<h1 style= "text-align: center; font-size: 36; color: violet; font-weight: bold">Bài tập về nhà đẩy code lên github</h1>';
+echo '<button style=" color: green;">Hai</button>';
+?>
+
+    
+</body>
+</html>
